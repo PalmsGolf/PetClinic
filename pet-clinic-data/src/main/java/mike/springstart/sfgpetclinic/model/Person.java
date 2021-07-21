1b8977e0f9b1f4d80ac29705bbf23904408ffaa4
m.kaminetskyi <m.kaminetskyi@levi9.com>
2021-07-21 13:20:41 +0300

    Adds impl based map services, code refactoring

--- pet-clinic-data/src/main/java/mike/springstart/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/mike/springstart/sfgpetclinic/model/Person.java
@@ -1,7 +1,7 @@
 package mike.springstart.sfgpetclinic.model;
 
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/mike/springstart/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/mike/springstart/sfgpetclinic/services/VetService.java
@@ -1,14 +1,10 @@
 package mike.springstart.sfgpetclinic.services;
 
+import mike.springstart.sfgpetclinic.model.Pet;
 import mike.springstart.sfgpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Pet, Long> {
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
