535c833674e5070b4312d0c5e40f179147b9840a
m.kaminetskyi <m.kaminetskyi@levi9.com>
2021-07-21 09:41:40 +0300

    Implements interfaces for services

---
+++ pet-clinic-data/src/main/java/mike/springstart/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package mike.springstart.sfgpetclinic.services;
+
+import mike.springstart.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/mike/springstart/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package mike.springstart.sfgpetclinic.services;
+
+import mike.springstart.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
